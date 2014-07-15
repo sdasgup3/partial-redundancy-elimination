1c252edd409c4e9cc3288e9018358aa6cf609a4f
Tanmay Gangwani <tanmay@ubuntu.(none)>
2014-07-14 22:03:47 -0500

    Revert ""
    This reverts commit 14b411187aa18544c97699b79adf2d669638b38e.

---
+++ Testing/Test_dfa/test_transp_3.c
@@ -0,0 +1,19 @@
+int func(int a, int b)
+{
+  int x,y,z,c,w;
+
+    if(x >= 0 ) {
+      x = a+b;
+      y = a-b;
+      z = x + y;
+    } else {
+      x = a+b;
+      y = a-b;
+      z = x + y;
+    }
+    z = x + y;
+
+
+
+  return z;
+}
